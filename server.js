3e8d1881817ce92cdef27652cd21d89689a7b480
rrahul08 <thefirstclass555@gmail.com>
2024-04-15 17:45:01 +0530

    created server for sked.com

---
+++ server.js
@@ -0,0 +1,68 @@
+const express = require('express')
+const app = express()
+const collection = require('./arango');
+const cors = require("cors")
+
+
+app.use(express.json())
+app.use(cors())
+
+
+
+
+app.get("/",cors(),(req,res)=>{
+
+})
+
+
+app.post("/login",async(req,res)=>{
+    const{email,password}=req.body
+
+    try{
+        const check=await collection.findOne({email:email})
+
+        if(check){
+            res.json("exist")
+        }
+        else{
+            res.json("notexist")
+        }
+
+    }
+    catch(e){
+        res.json("fail")
+    }
+
+})
+
+
+
+app.post("/signup",async(req,res)=>{
+    const{email,password}=req.body
+
+    const data={
+        email:email,
+        password:password
+    }
+
+    try{
+        const check=await collection.findOne({email:email})
+
+        if(check){
+            res.json("exist")
+        }
+        else{
+            res.json("notexist")
+            await collection.insertMany([data])
+        }
+
+    }
+    catch(e){
+        res.json("fail")
+    }
+
+})
+
+app.listen(8000,()=>{
+    console.log("port connected");
+})
